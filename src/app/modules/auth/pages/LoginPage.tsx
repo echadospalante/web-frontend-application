802553771a2fa8b6a084b12424db1810bc56d05f
Carolina Cardona <carolina.cardona@sourcemeridian.com>
2024-09-10 19:48:53 -0500

    avances en login

--- src/app/modules/auth/pages/LoginPage.tsx
+++ src/app/modules/auth/pages/LoginPage.tsx
@@ -1,187 +1,76 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
 
-import { CredentialResponse, useGoogleOneTapLogin } from "@react-oauth/google";
-import { Link, useNavigate } from "react-router-dom";
-import { Button, Card, CardBody, Col, Container, Row } from "reactstrap";
+// Formik validation
+import { GoogleLogin } from "@react-oauth/google";
 import useLogin from "../hooks/useLogin";
 
-// import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+// import images
 
 const LoginPage = () => {
-  document.title = "Login - Registro | Comercial";
-  const navigate = useNavigate();
   const { loginUser } = useLogin();
-  // const { instance } = useMsal();
-  // const isAuthenticated = useIsAuthenticated();
 
-  useGoogleOneTapLogin({
-    onSuccess: (credentialResponse) => {
-      handleSuccessfulLogin(credentialResponse);
-    },
-    onError: () => {
-      handleErrorInLogin();
-      console.log("Login Failed");
-    },
-  });
+  //meta title
+  document.title = "Login 2 | Skote - Vite React Admin & Dashboard Template";
 
-  const handleSuccessfulLogin = (
-    credentialResponse: CredentialResponse
-  ): void => {
-    const { credential } = credentialResponse;
-    if (!credential) {
-      return;
-    }
-    loginUser(credential);
-  };
-
-  const handleErrorInLogin = (): void => {
-    //
-  };
-
-  // const handleLogin = (loginType: "popup" | "redirect") => {
-  //   if (loginType === "popup") {
-  //     instance
-  //       .loginPopup({
-  //         scopes: ["openid", "profile", "User.Read"],
-  //         prompt: "select_account",
-  //       })
-  //       .catch((e) => {
-  //         console.log(e);
-  //       });
-  //   } else if (loginType === "redirect") {
-  //     instance
-  //       .loginRedirect({
-  //         scopes: ["openid", "profile", "User.Read"],
-  //         prompt: "select_account",
-  //       })
-  //       .catch((e) => {
-  //         console.log(e);
-  //       });
-  //   }
-  // };
-
-  const handleFakeLogin = (
-    to: "/principal" | "/administracion" | "/moderator"
-  ) => {
-    navigate(to, { replace: true });
-  };
+  // Form validation
 
   return (
-    <React.Fragment>
-      <div className="account-pages my-5 pt-sm-5">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md={8} lg={6} xl={5}>
-              <Card className="overflow-hidden">
-                <div className="bg-primary-subtle">
-                  <Row>
-                    <Col xs={7}>
-                      <div className="text-primary p-4">
-                        <h5 className="text-primary">
-                          Te damos la bienvenida!
-                        </h5>
-                        <p>
-                          Ingresa para continuar con tu cuenta de administrador.
-                        </p>
-                      </div>
-                    </Col>
-                    <Col className="col-5 align-self-end">
+    <div>
+      <Container fluid className="p-0">
+        <Row className="g-0">
+          <Col xl={6}>
+            <div className=" h-100 d-flex justify-content-center">
+              <img src="/Content-team.svg" width="80%" className="" alt="" />
+            </div>
+          </Col>
+          <Col xl={6} sm={12}>
+            <div className="auth-full-page-content p-md-5 p-4">
+              <div className="mx-auto">
+                <div className="d-flex flex-column h-100">
+                  <div className="mb-4 mb-md-5">
+                    <Link to="/" className="d-block auth-logo">
                       <img
-                        src="/images/profile-img.png"
                         alt=""
-                        className="img-fluid"
+                        src=""
+                        height="18"
+                        className="auth-logo-dark"
                       />
-                    </Col>
-                  </Row>
-                </div>
-                <CardBody className="pt-0">
-                  <div className="auth-logo">
-                    <Link to="/" className="auth-logo-light">
-                      <div className="avatar-md profile-user-wid mb-4">
-                        <span className="avatar-title rounded-circle bg-light">
-                          <img
-                            src="/favicon.ico"
-                            alt=""
-                            className="rounded-circle"
-                            height="34"
-                          />
-                        </span>
-                      </div>
-                    </Link>
-                    <Link to="/" className="auth-logo-dark">
-                      <div className="avatar-md profile-user-wid mb-4">
-                        <span className="avatar-title rounded-circle bg-light">
-                          <img
-                            src="/favicon.ico"
-                            alt=""
-                            className="rounded-circle"
-                            height="80"
-                          />
-                        </span>
-                      </div>
+                      <img
+                        alt=""
+                        src=""
+                        height="18"
+                        className="auth-logo-light"
+                      />
                     </Link>
                   </div>
-
-                  <Row className="p-2 d-flex flex-column justify-content-center">
-                    <Col lg={12} className="mt-3">
-                      <Button
-                        onClick={() => handleFakeLogin("/principal")}
-                        className="position-relative btn btn-soft-primary w-100 fw-medium border-light py-3"
-                        color="white"
-                      >
-                        <img
-                          src="/images/brands/google.svg"
-                          className="position-absolute"
-                          style={{ left: 10, top: 10 }}
-                        />
-                        <span className="text-center">
-                          Iniciar sesión con Google
-                        </span>
-                      </Button>
-                    </Col>
-                    <Col lg={12} className="mt-3">
-                      <Button
-                        onClick={() => handleFakeLogin("/moderator")}
-                        className="position-relative btn btn-soft-primary w-100 fw-medium border-light py-3"
-                        color="white"
-                      >
-                        <img
-                          src="/images/brands/microsoft.svg"
-                          className="position-absolute"
-                          style={{ left: 13, top: 15 }}
-                        />
-                        <span className="text-center">
-                          Iniciar sesión con Microsoft
-                        </span>
-                      </Button>
-                    </Col>
-
-                    <Col lg={12} className="mt-3">
-                      <Button
-                        onClick={() => handleFakeLogin("/administracion")}
-                        className="position-relative btn btn-soft-primary w-100 fw-medium border-light py-3"
-                        color="white"
-                      >
-                        <img
-                          src="/images/brands/linkedin.svg"
-                          className="position-absolute"
-                          style={{ left: 13, top: 13 }}
-                        />
-                        <span className="text-center">
-                          Iniciar sesión con LinkedIn
-                        </span>
-                      </Button>
-                    </Col>
-                  </Row>
-                  <div className="p-2 d-flex justify-content-center"></div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </React.Fragment>
+                  <div className="my-auto">
+                    <div>
+                      <img src="/logo-full.jpeg" width={300} alt="" />
+                      <h5 className="text-success mt-5">
+                        Te Damos la Bienvenida!
+                      </h5>
+                      <p className="text-muted">
+                        Ingresa con tu cuenta de Google
+                      </p>
+                    </div>
+                    <GoogleLogin
+                      onSuccess={(credentialResponse) => {
+                        if (!credentialResponse.credential) return;
+                        loginUser(credentialResponse.credential);
+                      }}
+                      onError={() => {
+                        console.log("Login Failed");
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
-
 export default LoginPage;
